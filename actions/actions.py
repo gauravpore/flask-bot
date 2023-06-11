1ea53bc00058d3ec0eb192959971e63c291a6c1e
gauravpore <gaurav@glideinvest.com>
2023-06-12 00:40:42 +0530

    rasa training files + other changes

--- actions/actions.py
+++ actions/actions.py
@@ -26,53 +26,53 @@
 #
 #         return []
 
-from typing import Any, Text, Dict, List
+# from typing import Any, Text, Dict, List
 
 
-from rasa_sdk import Action, Tracker
-from rasa_sdk.executor import CollectingDispatcher
-from rasa_sdk.events import SlotSet
+# from rasa_sdk import Action, Tracker
+# from rasa_sdk.executor import CollectingDispatcher
+# from rasa_sdk.events import SlotSet
 
 
-class ActionSavename(Action):
-    def name(self) -> Text:
-        return "action_save_name"
+# class ActionSavename(Action):
+#     def name(self) -> Text:
+#         return "action_save_name"
 
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
+#     def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
 
-        entity_name = tracker.latest_message.get("entities", [])
-        # Save the username or perform any other necessary processing
+#         entity_name = tracker.latest_message.get("entities", [])
+#         # Save the username or perform any other necessary processing
 
-        if entity_name:
-            name = entity_name[0].get("value")
-        else:
-            name = None  # Handle the case when no entity is present
+#         if entity_name:
+#             name = entity_name[0].get("value")
+#         else:
+#             name = None  # Handle the case when no entity is present
 
-        return [SlotSet("name", name)]
+#         return [SlotSet("name", name)]
 
 
-class ActionBeingLost(Action):
-    def name(self) -> Text:
-        return "action_being_lost"
+# class ActionBeingLost(Action):
+#     def name(self) -> Text:
+#         return "action_being_lost"
 
-    def run(
-        self,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: Dict[Text, Any],
-    ) -> List[Dict[Text, Any]]:
+#     def run(
+#         self,
+#         dispatcher: CollectingDispatcher,
+#         tracker: Tracker,
+#         domain: Dict[Text, Any],
+#     ) -> List[Dict[Text, Any]]:
 
-        entity_love_one = tracker.latest_message.get("entities", [])
-        # Save the username or perform any other necessary processing
+#         entity_love_one = tracker.latest_message.get("entities", [])
+#         # Save the username or perform any other necessary processing
 
-        if entity_love_one:
-            loved_one = entity_love_one[0].get("value")
-        else:
-            loved_one = None  # Handle the case when no entity is present
+#         if entity_love_one:
+#             loved_one = entity_love_one[0].get("value")
+#         else:
+#             loved_one = None  # Handle the case when no entity is present
 
-        return [SlotSet("loved_one", loved_one)]
+#         return [SlotSet("loved_one", loved_one)]
